268d05c13b2b71f6cec1c3f7642cd68277378914
Rushant Kangude <rushant.kangude728@gmail.com>
2024-12-27 16:41:44 +0530

    CCM

---
+++ CustomerCummunicationManagement/src/main/java/com/ccm/CustomerCummunicationManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ccm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerCummunicationManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerCummunicationManagementApplication.class, args);
+	}
+
+}
---
+++ CustomerCummunicationManagement/src/main/java/com/ccm/entity/Customer.java
@@ -0,0 +1,5 @@
+package com.ccm.entity;
+
+public class Customer {
+
+}
